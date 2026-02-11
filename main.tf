55f7ec1fb49e6e161f4539b84642511e8b2194b8
AshokGaniCloud <ashokkumarclouditdevcarrer6300@gmail.com>
2026-02-11 16:46:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
 
-  required_version = ">= 1.0.0"
-}
-
-data "vault_generic_secret" "azurecreds" {
-
-  path = "secret/azurecred"
-
-}
-
-provider "azurerm" {
-  features {}
-
-  client_id       = data.vault_generic_secret.azurecreds.data["azure_client_id"]
-  client_secret   = data.vault_generic_secret.azurecreds.data["azure_client_secret"]
-  tenant_id       = data.vault_generic_secret.azurecreds.data["azure_tenant_id"]
-  subscription_id = data.vault_generic_secret.azurecreds.data["azure_subscription_id"]
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = "vault-terrarg"
