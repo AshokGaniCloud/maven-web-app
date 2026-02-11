354e2aba3c3b52b6a58dc6202597875374da17c5
AshokGaniCloud <ashokkumarclouditdevcarrer6300@gmail.com>
2026-02-11 18:26:46 +0530

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+Cloud=AWS
+Tech=Devops
+
+echo "Currently you are working in this Technology is: $Tech"
+
+echo "We are using a cloud as $Cloud in my company."
+
+age=30
+
+if test $age -eq 30
+then
+        echo "you are eligible to vote."
+else
+        echo "you are not eligible to vote."
+fi
+echo "writing a Sample nested if..."
